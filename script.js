56cc0ed9acc21099ccb51cd9b6ab65eb4a0d5b5e
jack10242048 <jack10242048@gmail.com>
2026-03-12 09:37:46 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,27 @@ function draw(e) {
 function stopDraw() {
   drawing = false;
 }
+
+
+
+// ipad
+canvas.addEventListener("pointerdown", (e) => {
+  drawing = true;
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("pointermove", (e) => {
+  if (!drawing) return;
+
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+});
+
+canvas.addEventListener("pointerup", () => {
+  drawing = false;
+});
+
+canvas.addEventListener("pointerleave", () => {
+  drawing = false;
+});
